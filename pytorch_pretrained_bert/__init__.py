60150cb18ba6928d72c7a2ebffd5f4d3a591e3b0
dileep1996 <msaidileep@gmail.com>
2018-12-08 22:00:35 +0530

    Added AS model in __init__.py

--- pytorch_pretrained_bert/__init__.py
+++ pytorch_pretrained_bert/__init__.py
@@ -1,6 +1,6 @@
 from .tokenization import BertTokenizer, BasicTokenizer, WordpieceTokenizer
 from .modeling import (BertConfig, BertModel, BertForPreTraining,
                        BertForMaskedLM, BertForNextSentencePrediction,
-                       BertForSequenceClassification, BertForQuestionAnswering)
+                       BertForSequenceClassification, BertForQuestionAnswering, BertForAnswerSelection)
 from .optimization import BertAdam
 from .file_utils import PYTORCH_PRETRAINED_BERT_CACHE
